d413c00dcde8770d5257dfb610124f86e07583a7
zhahanger kydyraliyev <jkidiraliev@gmail.com>
2020-09-23 01:11:09 +0600

    Updates

---
+++ precache-manifest.07caa88a69e97597e79950e53875c7e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e808f381b4fe45330b5d28e375c8f6b0",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "2c3a89b8bd5eccadb70c",
+    "url": "/to-do/static/css/main.6703e45a.chunk.css"
+  },
+  {
+    "revision": "a89a9eea83312e0db451",
+    "url": "/to-do/static/js/2.cafcb383.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do/static/js/2.cafcb383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3a89b8bd5eccadb70c",
+    "url": "/to-do/static/js/main.eee7a68b.chunk.js"
+  },
+  {
+    "revision": "b5d210bec7b313800160",
+    "url": "/to-do/static/js/runtime-main.2b5910fc.js"
+  }
+]);
